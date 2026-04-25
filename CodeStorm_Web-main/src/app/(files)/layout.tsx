290c09b51d4513a848a240a9809dd0a3f29e3318
Mihai <mihaitelea02@gmail.com>
2026-04-25 11:20:51 +0300

    fix push?

--- CodeStorm_Web-main/src/app/(files)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="my-2 ml-2 h-[calc(100%-1rem)] overflow-x-hidden overflow-y-scroll rounded-3xl bg-zinc-900 text-zinc-100 [scrollbar-gutter:stable]">
-      <div className="p-10 pr-14">{children}</div>
-    </main>
-  );
-}
